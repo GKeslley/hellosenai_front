47a07f3341b977d9e3cfc200a5a28b1adeeb7a11
guilherme <guilhermekeslley123@gmail.com>
2023-10-09 18:33:32 -0300

    hook for verify screen size

---
+++ src/hooks/useMedia.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const useMedia = (media) => {
+  const [match, setMatch] = useState(null);
+
+  React.useEffect(() => {
+    const changeMatch = () => {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    };
+
+    changeMatch();
+
+    window.addEventListener('resize', changeMatch);
+
+    return () => {
+      window.removeEventListener('resize', changeMatch);
+    };
+  }, [media]);
+
+  return match;
+};
+
+export default useMedia;
