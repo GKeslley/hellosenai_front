445a814e50e3f539226515a96015367857106656
guilherme <guilhermekeslley123@gmail.com>
2023-11-14 18:13:14 -0300

    adding in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Invites from '../../components/Home/Invites'
-import Projects from '../../components/Home/Projects'
+import Invites from '../../components/Home/Invites';
+import Projects from '../../components/Home/Projects';
+import Advertising from '../../components/Home/Advertising';
+import Map from '../../components/Home/Map';
 
 const Home = () => {
   return (
-    <section className='flex flex-col gap-20'>
+    <section className="flex flex-col gap-24">
       <Projects />
       <Invites />
+      <Advertising />
+      <Map />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
