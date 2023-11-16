85cb674416dfba3413639d4c46e80c08a5c395bc
guilherme <guilhermekeslley123@gmail.com>
2023-11-16 17:55:21 -0300

    Adding refs to topics

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,28 @@
-import Invites from '../../components/Home/Invites';
 import Projects from '../../components/Home/Projects';
 import Advertising from '../../components/Home/Advertising';
 import Map from '../../components/Home/Map';
+import Topics from '../../components/Home/Topics';
+import { useRef } from 'react';
+import InvitesInfos from '../../components/Home/Invites/InvitesInfos';
 
 const Home = () => {
+  const refProject = useRef(null);
+  const refInvites = useRef(null);
+  const refAdvertising = useRef(null);
+  const refMap = useRef(null);
+
   return (
     <section className="flex flex-col gap-24">
-      <Projects />
-      <Invites />
-      <Advertising />
-      <Map />
+      <Topics
+        refProject={refProject}
+        refInvites={refInvites}
+        refAdversiting={refAdvertising}
+        refMap={refMap}
+      />
+      <Projects refProject={refProject} />
+      <InvitesInfos refInvites={refInvites} />
+      <Advertising refAdvertising={refAdvertising} />
+      <Map refMap={refMap} />
     </section>
   );
 };
