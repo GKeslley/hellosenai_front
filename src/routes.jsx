4cf1c1bc3c90df86256dce2037f76587f435f921
guilherme <guilhermekeslley123@gmail.com>
2023-11-17 21:27:50 -0300

    route for project

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Project from './pages/Project';
+import Projects from './pages/Projects';
 
 const CreateRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/projetos" element={<Project />}></Route>
+      <Route path="/projetos" element={<Projects />}></Route>
+      <Route path="/projetos/:slug" element={<Project />}></Route>
     </Routes>
   );
 };
