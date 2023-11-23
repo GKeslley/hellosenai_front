0c00e7805437c9beb1dcfe1ca2ae2aaaf61b631b
Guilherme <guilhermekeslley123@gmail.com>
2023-11-23 17:48:33 -0300

    Teacher login page v1

---
+++ src/pages/Login/LoginTeacher/index.jsx
@@ -0,0 +1,29 @@
+import { Box, Container, useMediaQuery } from '@mui/material';
+import teacherImage from '../../../assets/login/teacher.png';
+import LoginForm from '../../../components/Login/LoginForm';
+
+const LoginTeacher = () => {
+  const isMobile = useMediaQuery('(min-width: 768px)');
+
+  return (
+    <Container
+      component="section"
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: isMobile ? '1fr 1fr' : '1fr',
+        gap: '1rem',
+        marginBottom: '2rem',
+      }}
+    >
+      {isMobile && (
+        <Box component="figure">
+          <Box component="img" src={teacherImage} alt="Estudantes" />
+        </Box>
+      )}
+
+      <LoginForm />
+    </Container>
+  );
+};
+
+export default LoginTeacher;
