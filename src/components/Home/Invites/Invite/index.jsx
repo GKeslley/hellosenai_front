2759c4a332b14adb1550b35df0394bd730be2288
Guilherme <guilhermekeslley123@gmail.com>
2023-12-04 17:37:34 -0300

    get unique invite

--- src/components/Home/Invites/Invite/index.jsx
+++ src/components/Home/Invites/Invite/index.jsx
@@ -5,27 +5,50 @@ import {
   CardActionArea,
   CardActions,
   Box,
+  Grid,
 } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import './styles.css';
 import ButtonComponent from '../../../Button';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Invite = ({ modalAccessInvite }) => {
+const Invite = ({
+  modalAccessInvite,
+  title,
+  date,
+  author,
+  description,
+  setDataInvite,
+}) => {
+  const handleClick = () => {
+    setDataInvite({ titulo: title, data: date, autor: author, descricao: description });
+  };
+
   return (
-    <Card sx={{ minWidth: 245, flex: '1' }}>
+    <Grid
+      component={Card}
+      item
+      xs
+      minWidth="250px"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      onClick={handleClick}
+    >
       <CardActionArea>
         <CardContent>
           <Box className="flex items-center gap-x-4 mb-4">
             <Avatar sx={{ width: '30px', height: '30px' }} />
-            <Typography>Fulano</Typography>
+            <Link to={`/usuario/${author.apelido}`}>{author.nome}</Link>
           </Box>
-          <Typography gutterBottom variant="h5" fontWeight="500">
-            Facebook
+          <Typography gutterBottom fontSize="1.2rem" fontWeight="500">
+            {title}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions sx={{justifyContent: 'end'}}>
+      <CardActions sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+        <Typography fontSize="0.72rem">Postado em: {date}</Typography>
         <ButtonComponent
           variant="outlined"
           sx={{ justifySelf: 'right', maxWidth: 'max-content' }}
@@ -35,12 +58,17 @@ const Invite = ({ modalAccessInvite }) => {
           Acessar
         </ButtonComponent>
       </CardActions>
-    </Card>
+    </Grid>
   );
 };
 
 Invite.propTypes = {
   modalAccessInvite: PropTypes.func,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  date: PropTypes.string,
+  author: PropTypes.object,
+  setDataInvite: PropTypes.func,
 };
 
 export default Invite;
