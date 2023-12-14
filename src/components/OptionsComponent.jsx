5be989e33e94d8ac6ea995fcb1f0dceee199aa83
Guilherme <guilhermekeslley123@gmail.com>
2023-12-14 18:05:23 -0300

    options component

---
+++ src/components/OptionsComponent.jsx
@@ -0,0 +1,43 @@
+import { Box, IconButton, Menu } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import PropTypes from 'prop-types';
+
+const OptionsComponent = ({ children, onClick, anchorEl, handleClose }) => {
+  const open = Boolean(anchorEl);
+
+  return (
+    <Box>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={onClick}
+      >
+        <MoreHorizIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        sx={{ gap: '0.5rem' }}
+      >
+        {children}
+      </Menu>
+    </Box>
+  );
+};
+
+OptionsComponent.propTypes = {
+  children: PropTypes.array,
+  onClick: PropTypes.func,
+  anchorEl: PropTypes.bool,
+  handleClose: PropTypes.func,
+};
+
+export default OptionsComponent;
