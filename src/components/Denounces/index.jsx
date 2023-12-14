0f245af7874819f788e7fc2e61581b038e641da1
Guilherme <guilhermekeslley123@gmail.com>
2023-12-14 18:04:42 -0300

    changing denounces table

--- src/components/Denounces/index.jsx
+++ src/components/Denounces/index.jsx
@@ -1,5 +1,16 @@
-import { Box, Button, Container, Typography } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableBody,
+} from '@mui/material';
+
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import Subtitle from '../Subtitle';
@@ -7,17 +18,8 @@ import WarningIcon from '@mui/icons-material/Warning';
 import { useState } from 'react';
 import ModalComponent from '../Modal';
 import { Link } from 'react-router-dom';
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'texto', headerName: 'Texto', width: 500 },
-  { field: 'projeto', headerName: 'Projeto', width: 250 },
-  {
-    field: 'criada_em',
-    headerName: 'Data Criação',
-    width: 150,
-  },
-];
+import Loading from '../Helper/Loading';
+import ButtonComponent from '../Button';
 
 const Denounces = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -26,67 +28,96 @@ const Denounces = () => {
     headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
   };
 
-  columns[4] = {
-    headerName: 'Visualizar',
-    width: 200,
-    // Define a custom render function for the cell
-    renderCell: (params) => {
-      const row = params.row;
-      const handleClick = () => {
-        setDenounce(row);
-        setOpenModal(true)
-      };
-      return (
-        <Button onClick={handleClick} variant="contained" size="small">
-          Visualizar
-        </Button>
-      );
-    },
-  };
-
   console.log(denounce);
 
-  const { data, isLoading, error } = useQuery('denounces', () => {
+  const { data, isLoading } = useQuery('denounces', () => {
     return axios
       .get('http://127.0.0.1:8000/api/v1/denuncia', config)
       .then((response) => response.data);
   });
 
+  const visualiseDenounce = (data) => {
+    setDenounce(data);
+    setOpenModal(true);
+  };
   console.log(data);
 
+  if (isLoading) return <Loading />;
   return (
     <Container sx={{ marginTop: '2rem', marginBottom: '2rem' }}>
       <Subtitle>
         Denúncias Recentes
         <WarningIcon sx={{ width: '30px', height: '30px', marginLeft: '0.25rem' }} />
       </Subtitle>
-      {data && (
-        <div style={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={data.data}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: data.data.length },
-              },
-            }}
-            pageSizeOptions={[5, 10]}
-          />
-        </div>
-      )}
+      <TableContainer>
+        <Table sx={{ minWidth: 450 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Projeto</TableCell>
+              <TableCell align="right">Data Criação</TableCell>
+              <TableCell align="right">Visualizar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.data.map(({ id, texto, projeto, criada_em }) => (
+              <TableRow
+                key={id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {id}
+                </TableCell>
+                <TableCell align="right">{projeto}</TableCell>
+                <TableCell align="right">{criada_em}</TableCell>
+                <TableCell align="right">
+                  <ButtonComponent
+                    variant="outlined"
+                    onClick={() => visualiseDenounce({ id, texto, projeto, criada_em })}
+                  >
+                    Visualizar
+                  </ButtonComponent>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
-      {denounce && 
+      {denounce && (
         <ModalComponent openModal={openModal} setOpenModal={setOpenModal}>
-          <Box sx={{display: 'flex', flexDirection: 'column', padding: '1rem', gap: '1rem'}}>
-            <Typography fontSize='1.5rem'>Denúncia feita em {denounce.criada_em}</Typography>
-            <Box sx={{display: 'flex', flexDirection: 'column', gap: '0.5rem'}}>
-              <Typography>Denúncia: {denounce.texto}</Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: '1rem',
+              gap: '1rem',
+            }}
+          >
+            <Typography fontSize="1.5rem">
+              Denúncia feita em {denounce.criada_em}
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+              <Typography
+                sx={{
+                  maxWidth: 'max-content',
+                  whiteSpace: 'break-spaces',
+                  wordBreak: 'break-word',
+                }}
+              >
+                Denúncia: {denounce.texto}
+              </Typography>
             </Box>
-            <Box component={Link} to={`/projetos/${denounce.projeto}`} sx={{textDecoration: 'underline', width: 'max-content'}}>
+            <Box
+              component={Link}
+              to={`/projetos/${denounce.projeto}`}
+              sx={{ textDecoration: 'underline', width: 'max-content' }}
+            >
               Visualizar Projeto
             </Box>
-          </Box>  
-        </ModalComponent>}
+          </Box>
+        </ModalComponent>
+      )}
     </Container>
   );
 };
