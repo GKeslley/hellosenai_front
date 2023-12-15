dc8efa858c14b4ca0b9649cc6d43bcfd0d113721
Guilherme <guilhermekeslley123@gmail.com>
2023-12-15 18:05:28 -0300

    adm page

---
+++ src/pages/Adm/Register/index.jsx
@@ -0,0 +1,121 @@
+import { Box, Container, FormControl } from '@mui/material';
+import Title from '../../../components/Title';
+import Input from '../../../components/Form/Input';
+import useForm from '../../../hooks/useForm';
+import ButtonComponent from '../../../components/Button';
+import { useMutation } from 'react-query';
+import axios from 'axios';
+import { useState } from 'react';
+import SnackbarRequest from '../../../components/SnackbarRequest';
+
+const RegisterAdm = () => {
+  const [openSnackbar, setOpenSnackbar] = useState({
+    open: false,
+    message: '',
+    severity: '',
+  });
+  const name = useForm(true);
+  const email = useForm('email');
+  const password = useForm('password');
+
+  const mutation = useMutation({
+    mutationFn: ({ data, token }) => {
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+
+      return axios.post('http://127.0.0.1:8000/api/v1/adm', data, config);
+    },
+    onSuccess: ({ data }) => {
+      name.setValue('');
+      email.setValue('');
+      password.setValue('');
+      setOpenSnackbar({ open: true, message: data.message, severity: 'success' });
+    },
+    onError: (error) => {
+      setOpenSnackbar({
+        open: true,
+        message: error.response.data.message,
+        severity: 'error',
+      });
+    },
+  });
+
+  const sendMessage = () => {
+    if (name.validate() && email.validate()) {
+      const data = {
+        nome: name.value,
+        email: email.value,
+        senha: password.value,
+      };
+      const token = localStorage.getItem('token');
+      mutation.mutate({ data, token });
+    }
+  };
+
+  return (
+    <Box sx={{ flexGrow: '1', position: 'relative' }}>
+      <Container sx={{ marginTop: '2rem', marginBottom: '2rem' }}>
+        <Title sx={{ marginBottom: '2rem' }}>Novo Administrador</Title>
+        <FormControl
+          sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '1rem' }}
+        >
+          <Input
+            error={name.error.isError}
+            required={true}
+            id="name"
+            fullWidth={true}
+            value={name.value}
+            onChange={name.onChange}
+            onBlur={name.onBlur}
+            helperText={name.error.isError && name.error.message}
+            label="Nome"
+            variant="standard"
+          />
+          <Input
+            error={email.error.isError}
+            required={true}
+            id="email"
+            fullWidth={true}
+            value={email.value}
+            onChange={email.onChange}
+            onBlur={email.onBlur}
+            helperText={email.error.isError && email.error.message}
+            label="Email"
+            variant="standard"
+          />
+          <Input
+            error={password.error.isError}
+            required={true}
+            id="password"
+            fullWidth={true}
+            value={password.value}
+            onChange={password.onChange}
+            onBlur={password.onBlur}
+            helperText={password.error.isError && password.error.message}
+            label="Senha"
+            variant="standard"
+            type="password"
+          />
+          <ButtonComponent
+            onClick={sendMessage}
+            sx={{ marginBottom: '1rem' }}
+            isLoading={mutation.isLoading}
+          >
+            Enviar
+          </ButtonComponent>
+        </FormControl>
+        {openSnackbar.open && (
+          <SnackbarRequest
+            message={openSnackbar.message}
+            open={openSnackbar.open}
+            onClose={() => setOpenSnackbar({ open: false, message: '', severity: '' })}
+            severity={openSnackbar.severity}
+          />
+        )}
+      </Container>
+    </Box>
+  );
+};
+
+export default RegisterAdm;
--- src/pages/Adm/index.jsx
+++ src/pages/Adm/index.jsx
@@ -1,6 +1,7 @@
 import { Avatar, Box, Container, Paper, Typography } from '@mui/material';
 import teacherImage from '../../assets/login/pvta.png';
 import warningImage from '../../assets/teste2.png';
+import adm from '../../assets/login/sla.png';
 import Title from '../../components/Title';
 import { Link } from 'react-router-dom';
 
@@ -72,6 +73,35 @@ const Adm = () => {
           />
           <Typography fontSize="1.5rem" fontWeight="500">
             Denúncias
+          </Typography>
+        </Paper>
+
+        <Paper
+          component={Link}
+          to="/adm/registrar"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '1rem 1rem',
+            alignItems: 'center',
+            gap: '0.5rem',
+            minWidth: { xs: '15rem', sm: '15rem' },
+            maxWidth: { xs: '15rem', sm: '15rem' },
+          }}
+        >
+          <Avatar
+            src={adm}
+            sx={{
+              objectPosition: 'top',
+              width: '90px',
+              height: '90px',
+              '& .MuiAvatar-img': {
+                objectPosition: 'top',
+              },
+            }}
+          />
+          <Typography fontSize="1.5rem" fontWeight="500">
+            Novo ADM
           </Typography>
         </Paper>
       </Box>
